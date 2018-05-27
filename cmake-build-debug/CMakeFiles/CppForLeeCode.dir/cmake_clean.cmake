2a2c28afbd4c39a948bcac53c01c51eeeed3cc0b
WindAsMe <542636539@qq.com>
2018-05-27 15:26:25 +0800

    Cpp For LeeCode

---
+++ cmake-build-debug/CMakeFiles/CppForLeeCode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppForLeeCode.dir/main.cpp.o"
+  "CppForLeeCode.pdb"
+  "CppForLeeCode"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppForLeeCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
