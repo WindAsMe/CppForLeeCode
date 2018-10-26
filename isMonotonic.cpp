d591651d9fb359df892952ffd15774b7a26740e3
WindAsMe <542636539@qq.com>
2018-10-26 18:55:48 +0800

    Cpp For LeetCode

--- isMonotonic.cpp
+++ isMonotonic.cpp
@@ -11,17 +11,21 @@ using namespace std;
 class Solution {
 public:
     bool isMonotonic(vector<int>& A) {
-        int ans = 1;
+        vector<int> helper;
         for (int i = 1; i < A.size(); i++)
-            if (A[i] - A[i - 1] != 0)
-                ans *= A[i] - A[i - 1];
-        return ans > 0;
+            if (A[i] - A[i - 1])
+                helper.push_back(A[i] - A[i - 1]);
+        for (int i = 1; i < helper.size(); i++) {
+            if (helper[i] * helper[i - 1] < 0)
+                return false;
+        }
+        return true;
     }
 };
 
 int main() {
     Solution s;
-    int a[] = {1,2,4,3};
+    int a[] = {5,4,3,1};
     vector<int> v(a, a + static_cast<int>(end(a) - begin(a)));
     cout << s.isMonotonic(v);
 }
